6f90325d05c67f463cc9dcee86286e0fc3287636
Jose M. <jmcunat@syntetyk.com>
2017-02-17 12:09:50 +0100

    Corrección de espacio para en vivo con titular con más líneas de texto

--- src/providers/episodios-service.ts
+++ src/providers/episodios-service.ts
@@ -11,21 +11,21 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class EpisodiosService {
-    
+
     static get parameters() {
         return [[Http]];
     }
-    
+
     constructor(public http: Http) {
         //console.log('Hello EpisodiosService Provider');
     }
-  
+
     dameEpisodios(){
         var episodiosJSON = this.http.get('https://api.spreaker.com/v2/shows/1060718/episodes').map(res => res.json()); // Fernando
         //var episodiosJSON = this.http.get('https://api.spreaker.com/v2/shows/1341125/episodes').map(res => res.json());   //Live
         return episodiosJSON;
     }
-  
+
     dameEpisodios2(){ //Esto se usa para pruebas....
         var episodiosJSON = this.http.get('https://api.spreaker.com/v2/shows/1060718/episodes?filter=listenable&last_id=10462689').map(res => res.json()); // Fernando
         //var episodiosJSON = this.http.get('https://api.spreaker.com/v2/shows/1341125/episodes').map(res => res.json());   //Live
@@ -38,17 +38,17 @@ export class EpisodiosService {
         //var episodiosJSON = this.http.get('https://api.spreaker.com/v2/shows/1341125/episodes').map(res => res.json());   //Live
         return episodiosJSON;
     }
-  
+
     dameDetalleEpisodio(episodio_id){
         var episodiosJSON = this.http.get('https://api.spreaker.com/v2/episodes/'+ episodio_id).map(res => res.json());
         return episodiosJSON;
     }
-  
+
     dameChatEpisodio(episodio_id){
         var episodiosJSON = this.http.get('https://api.spreaker.com/v2/episodes/'+ episodio_id+'/messages').map(res => res.json());
         return episodiosJSON;
     }
-  
+
     dameMasComentarios(episodio_id, ultimo_comentario){
         console.log("[EPISODIOS-SERVICE] Solicitados comentarios del episodio "+ episodio_id +" más allá del "+ ultimo_comentario  );
         var episodiosJSON = this.http.get('https://api.spreaker.com/v2/episodes/'+ episodio_id+'/messages?last_id='+ultimo_comentario).map(res => res.json());
