ef785eb3a36e5fe0f4099e104f7d6b7f7a6f9a69
a930334 <josecarlos.santosruiz@gmail.com>
2017-04-23 14:54:48 +0200

    Add files via upload
    Cambio a Ionic 3.0
    Login a Spreaker

---
+++ configuracion.service.ts
@@ -0,0 +1,205 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/Rx';
+import { Storage } from '@ionic/storage';
+
+import { EpisodiosService } from "./episodios-service";
+
+
+@Injectable()
+
+export class ConfiguracionService {
+    // typing our private Observable, which will store our chosen theme in-memory
+    public theme: BehaviorSubject<String>;
+    // as promised, I've moved the availableThemes here as well
+    availableThemes: {className: string, prettyName: string}[];
+
+    constructor(public storage: Storage, public episodioSrvc: EpisodiosService) {
+        this.theme = new BehaviorSubject('tema-base');
+
+        // again, hard-coding the values for possible selections,
+        // but you would similarly pull this from your database as well
+        this.availableThemes = [
+            {className: 'tema-base', prettyName: 'Día'},
+            {className: 'tema-bosque', prettyName: 'Bosque'},
+            {className: 'tema-cafe', prettyName: 'Café'},
+            {className: 'tema-lila', prettyName: 'Lila'},
+            {className: 'tema-noche', prettyName: 'Noche'}
+        ];
+
+        this.storage.get('tema')
+        .then((val) => {
+            //console.log("[CONFIGURACION.SERVICE] El tema guardado es "+ val);
+            if (this.availableThemes.findIndex((element)=>{
+                    return (element.className ===val);
+                }) > -1) {
+                //this.theme = new BehaviorSubject(val);
+                //console.log("[CONFIGURACION.SERVICE] El tema guardado es correcto. Lo envío");
+                this.theme.next(val);
+                //this.theme.next(val);
+            }
+            else {
+                //console.log("[CONFIGURACION.SERVICE] El tema guardado no lo reconozco. Envío el tema por defecto");
+                //this.theme = new BehaviorSubject('tema-base');
+                //this.theme.next('tema-base');
+            }
+        })
+        .catch((error)=>{
+            console.log("[CONFIGURACION.SERVICE] Error enviando: " + error);
+        });
+    }
+
+    setTheme(val) {
+        if (val == null){
+            console.log("[CONFIGURACION_SERVICE.setTheme] No puedo guardar un valor nulo")
+        }
+        else {
+            this.storage.set ("tema",val);
+            this.theme.next(val);
+        }
+        //console.log("[CONFIGURACION.SERVICE.setTheme] cambiado el tema a "+ val );
+    }
+
+    getTheme() {
+        //console.log("[CONFIGURACION.SERVICE.getTheme] Enviado tema.")
+        //return this.theme.asObservable();
+        this.storage.get ("tema")
+        .then (
+            data=> {return this.theme.next(data)},
+            error=> {return this.theme.next('tema-base')}
+        )
+    }
+
+    getWIFI(){
+        //console.log("[CONFIGURACION.SERVICE.getWIFI].")
+        //return this.theme.asObservable();
+        return (this.storage.get ("WIFI"));/*
+        return new Promise ((resolve,reject) =>{
+             this.storage.get ("WIFI")
+            .then (
+                data=> {
+                    console.log ("[CONFIGURACION.SERVICE.getWIFI] Enviado "+ data);
+                    resolve(data==null?false:data);
+                },
+                error=> {
+                    console.log ("[CONFIGURACION.SERVICE.getWIFI] Error "+ error);
+                    resolve(false);
+                }
+        )});*/
+    }
+
+    setWIFI(val:boolean){
+        if (val == null){
+            console.log("[CONFIGURACION_SERVICE.setWIFI] No puedo guardar un valor nulo")
+        }
+        else {
+            this.storage.set ("WIFI",val);
+        //console.log("[CONFIGURACION.SERVICE.setWIFI] cambiado el WIFI a "+ val );
+        }
+    }
+
+    setTimeRep (cap: string, pos:number){
+        if (cap == null || pos == null){
+            console.log("[CONFIGURACION_SERVICE.setTimeRep] No puedo guardar un valor nulo")
+        }
+        else {
+        //console.log("[CONFIGURACION.SERVICE.setTimeRep] Guardando posición del cap. " + cap + ": " + pos);
+            this.storage.set ("pos_"+cap, pos);
+        }
+    }
+
+    getTimeRep(cap: string){
+        console.log("[CONFIGURACION.SERVICE.getTimeRep] Recuperando posición del cap. " + cap + ".");
+        //return this.theme.asObservable();
+        return new Promise ((resolve,reject) =>{
+
+            if (cap == null){
+                console.log("[CONFIGURACION_SERVICE.getTimeRep] No puedo guardar un valor nulo");
+                reject("No puedo devolver la posición de un capítulo vacío.")
+            }
+            else {
+                this.storage.get ("pos_"+cap)
+                .then (
+                    data=> {
+            //           console.log ("[CONFIGURACION.SERVICE.getTimeRep] Enviado "+ data);
+                        resolve(data);
+                    },
+                    error=> {
+                        console.log ("[CONFIGURACION.SERVICE.getTimeRep] Error "+ error);
+                        resolve(0);
+                    }
+            )}});
+    }
+
+    setTwitteado (cap: string){
+        //console.log("[CONFIGURACION.SERVICE.setTwitteado] Guardando capítulo twitteado para " + cap);
+        if (cap == null){
+            console.log("[CONFIGURACION_SERVICE.setTwitteado] No puedo guardar un valor nulo")
+        }
+        else {
+            this.storage.set ("Twit_"+cap, true); // la cosa es guardar si se ha preguntado el twitear este capítulo; no qué ha respondido.
+        }
+    }
+
+    getTwitteado (cap: string){
+       // console.log("[CONFIGURACION.SERVICE.getTimeRep] Recuperando posición del cap. " + cap + ".");
+        //return this.theme.asObservable();
+        return new Promise ((resolve,reject) =>{
+
+            if (cap == null){
+                console.log("[CONFIGURACION_SERVICE.getTwitteado] No puedo guardar un valor nulo");
+                reject("No puedo devolver si se ha twitteado un capítulo vacío.")
+            }
+            else {
+                this.storage.get ("Twit_"+cap)
+                .then (
+                    data=> {
+            //            console.log ("[CONFIGURACION.SERVICE.getTwitteado] Enviado "+ data);
+                        resolve(data);
+                    },
+                    error=> {
+                        console.log ("[CONFIGURACION.SERVICE.getTwitteado] Error "+ error);
+                        resolve(0);
+                    }
+                )
+            }
+        });
+    }
+
+    setTokenSpreaker (token: string){
+        console.log("[CONFIGURACION.SERVICE.setTokenSpreaker] Guardando token login spreaker " + token);
+        this.storage.set ("tokenSpreaker", token);
+    }
+
+    getTokenSpreaker ():Promise<any>{
+        return new Promise ((resolve,reject) =>{
+            this.storage.get ("tokenSpreaker")
+            .then ((data) => {
+                    console.log ("[CONFIGURACION.SERVICE.getTokenSpreaker] Enviado "+ data);
+                    if (data != ""){
+                        console.log ("[CONFIGURACION.SERVICE.getTokenSpreaker] solicitando datos de usuario.");
+                        this.episodioSrvc.whoAMi(data).subscribe(
+                            data => {
+                                console.log("[CONFIGURACION.SERVICE.getTokenSpreaker] recibido " + data.response.user);
+                                this.storage.set ("usuarioSpreaker", data.response.user.username);
+                            },
+                            err => {
+                                console.log("[CONFIGURACION.SERVICE.getTokenSpreaker] Error solicitando datos de usuario:" + err);
+                            }
+                        )
+                    }
+                    resolve(data);
+                }
+            )
+            .catch ((error) => {
+                console.log ("[CONFIGURACION.SERVICE.getTokenSpreaker] Error recuperando tokenSpreaker "+ error);
+                this.storage.set ("usuarioSpreaker", "");
+                resolve(0);
+            }); 
+        });
+    }
+
+    dameUsuario ():Promise<any>{
+        return (this.storage.get ("usuarioSpreaker"));
+    }
+
+}
---
+++ info-fer.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { MediaPlugin, MediaObject } from '@ionic-native/media';
+import { Platform } from 'ionic-angular';
+import { Dialogs } from '@ionic-native/dialogs';
+
+/*
+  Generated class for the InfoFer page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-info-fer',
+  templateUrl: 'info-fer.html',
+  providers: [Dialogs]
+})
+export class InfoFerPage {
+    
+    repPlugin = new MediaPlugin ();
+    dirJingle:MediaObject;
+    reproduciendo: boolean = false;
+    esAndroid:boolean;
+    
+    constructor(public navCtrl: NavController, public navParams: NavParams, public platform : Platform, private mensaje: Dialogs) {
+        this.repPlugin.create ('http://www.xpress.es/radiocable/audio/jingle-cafetera-tennesse.mp3')
+            .then((objeto)=> {
+                this.dirJingle = objeto;
+            })
+            .catch((error)=> {
+                console.log("[PLAYER.crearepPlugin] Error creando reproductor:"+ error); 
+            });
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad InfoFerPage');
+     /*   this.esAndroid = this.platform.is('android');
+     //   if(this.esAndroid){
+            this.dirJingle.init.then(() => {
+                console.log('[INFO_FER.ionViewDidLoad] Audio terminado');
+            }, (err) => {
+                console.log('[INFO_FER.ionViewDidLoad] Algo falló. Código de error: ' + err.code + '; mensaje: ' + err.message);
+            });
+        }*/
+    }
+
+    ngOnDestroy(){
+        if (this.dirJingle != null) {
+            this.dirJingle.play();
+        }
+    }
+    
+    reproduceJingle (){
+        if (this.dirJingle != null){
+            if (this.reproduciendo) {
+         //       if (this.esAndroid){
+                    this.dirJingle.pause();
+         //       }
+                this.reproduciendo=!this.reproduciendo;
+            }
+            else {
+         //       if (this.esAndroid){
+                    this.dirJingle.play();
+         //       }
+                this.mensaje.alert("El grupo JAVA no se hace responsable de la reproducción de este jingle", 'Super-Gurú.');
+                this.reproduciendo=!this.reproduciendo;
+            }
+        }
+        else alert("Es nulo");
+    }
+}
