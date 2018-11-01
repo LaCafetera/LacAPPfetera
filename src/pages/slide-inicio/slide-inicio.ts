3c3c5dc2509e67cca20962a97043e01d8a8dac36
José Carlos <josecarlos.santosruiz@gmail.com>
2018-11-01 14:48:12 +0100

    Revisión de presentación inicial.

--- src/pages/slide-inicio/slide-inicio.ts
+++ src/pages/slide-inicio/slide-inicio.ts
@@ -19,6 +19,7 @@ export class SlideInicioPage {
 
   horizontal :boolean = false;
   movil : boolean = true;
+  ulti : boolean = false;
 
   @ViewChild(Slides) slides: Slides;
 
@@ -26,23 +27,33 @@ export class SlideInicioPage {
     {titulo: "Diapo1",
      descripcion: "Paseaba @radiocable por la calle...",
      imagenV: "assets/images/SI1V.jpg",
-     imagenH: "assets/images/SI1H.jpg"},
+     imagenH: "assets/images/SI1H.jpg",
+     boton: false},
     {titulo: "Diapo2",
     descripcion: "...cuando se encontró con Inda que le miraba golosón...",
     imagenV: "assets/images/SI2V.jpg",
-    imagenH: "assets/images/SI2H.png"},
+    imagenH: "assets/images/SI2H.png",
+    boton: false},
     {titulo: "Diapo3",
     descripcion: "Tras el susto inicial...",
     imagenV: "assets/images/SI3V.jpg",
-    imagenH: "assets/images/SI3H.png"},
+    imagenH: "assets/images/SI3H.png",
+    boton: false},
     {titulo: "Diapo4",
     descripcion: "... vino la reflexión sosegada:",
     imagenV: "assets/images/SI4V.jpg",
-    imagenH: "assets/images/SI4H.png"},
+    imagenH: "assets/images/SI4H.png",
+    boton: false},
     {titulo: "Diapo5",
     descripcion: "¡Podría haber sido peor! :-D ",
     imagenV: "assets/images/SI5V.jpg",
-    imagenH: "assets/images/SI5H.png"}
+    imagenH: "assets/images/SI5H.png",
+    boton: false},
+    {titulo: "Diapo6",
+    descripcion: "",
+    imagenV: "assets/images/SI6H.png",
+    imagenH: "assets/images/SI6V.png",
+    boton: true}
   ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private screenOrientation: ScreenOrientation, public _platform: Platform) {
@@ -58,7 +69,7 @@ export class SlideInicioPage {
             this.horizontal = this.screenOrientation.type.includes ("landscape");
           },
           err => {
-            console.error ("La orientación del terminal es: " + this.screenOrientation.type + " - " + JSON.stringify(err) );
+            console.error ("[SlideInicioPage.ionViewDidLoad] La orientación del terminal es: " + this.screenOrientation.type + " - " + JSON.stringify(err) );
           }
         );
       })
@@ -66,6 +77,11 @@ export class SlideInicioPage {
 
   letsRock(){
     this.navCtrl.setRoot (HomePage);
+  }
+
+  slideChanged(){
+    this.ulti = this.slides.getActiveIndex()==this.slides.length()-1;
+    console.log ("Activo " +  this.slides.getActiveIndex() + " longitud " + this.slides.length() + " ulti vale " + this.ulti);
   }
 
 }
